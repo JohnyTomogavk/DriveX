b298c6c81755f79141aac049111e2f75578abd69
Ivan Sachishin <i.sachishin@gmail.com>
2025-03-13 00:52:22 +0300

    Configure eslint.

--- src/drivex-web-ui/src/app/app.component.ts
+++ src/drivex-web-ui/src/app/app.component.ts
@@ -8,5 +8,13 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'drivex-web-ui';
+  private a = 5;
+
+  public b = 5;
+
+  title = 'DriveX';
+
+  test() {
+    console.log("test")
+  }
 }
